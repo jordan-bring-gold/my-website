914b89f9b6e80f3d3b26bc25db6fe62da2da5a01
jordan-bring-gold <77407047+jordan-bring-gold@users.noreply.github.com>
2026-02-15 09:49:45 -0600

    Fixed Missing Tag

--- layout.tsx
+++ layout.tsx
@@ -34,9 +34,18 @@ export default function RootLayout({
           rel="stylesheet"
         />
         {/* <link rel="icon" href="/favicon.svg" type="image/svg+xml" /> */}
-        <link rel="icon" href="/favicon-dark.svg"  type="image/svg+xml" media="(prefers-color-scheme: light)">
-        <link rel="icon" href="/favicon-light.svg"  type="image/svg+xml" media="(prefers-color-scheme: dark)">
-
+        <link
+          rel="icon"
+          href="/favicon-dark.svg"
+          type="image/svg+xml"
+          media="(prefers-color-scheme: light)"
+        />
+        <link
+          rel="icon"
+          href="/favicon-light.svg"
+          type="image/svg+xml"
+          media="(prefers-color-scheme: dark)"
+        />
       </head>
       <body className={cn("min-h-screen font-body antialiased flex flex-col")}>
         <Header />
